12e389191ac50d9d6b739133d18a5082d296c8b5
rascal <nalpa199858@gmail.com>
2023-12-05 13:43:58 -0800

    update delete version and add prompt delete direct

--- src/utils/chatStream.ts
+++ src/utils/chatStream.ts
@@ -24,7 +24,7 @@ export const OpenAIStream = async (
   const prompt = createPrompt(inputCode);
   const direct_prompt = "You are amazon seller assistant. You will try to respond to user's questions, but you get easily distracted.";
 
-  const system = { role: 'system', content: direct_prompt + prompt };
+  const system = { role: 'system', content:  prompt };
   console.log('system-->', system);
 
   const res = await fetch(`https://api.openai.com/v1/chat/completions`, {
